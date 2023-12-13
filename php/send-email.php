afaf122e4a9154d84f3717b5f8f0ea5b8c6e6f24
abdullahnaseer01 <mianabdullah125125@gmail.com>
2023-12-13 21:01:52 +0500

    refactoring

--- php/send-email.php
+++ php/send-email.php
@@ -3,46 +3,53 @@
 // Replace this with your own email address
 $to = 'abdullah137751@gmail.com';
 
-function url(){
-  return sprintf(
-    "%s://%s",
-    isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
-    $_SERVER['SERVER_NAME']
-  );
+// Function to get the current URL
+function getCurrentURL() {
+    return sprintf(
+        "%s://%s%s",
+        isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+        $_SERVER['HTTP_HOST'],
+        $_SERVER['REQUEST_URI']
+    );
 }
 
-if($_POST) {
+// Check if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-   $name = trim(stripslashes($_POST['name']));
-   $email = trim(stripslashes($_POST['email']));
-   $subject = trim(stripslashes($_POST['subject']));
-   $contact_message = trim(stripslashes($_POST['message']));
+    // Sanitize and trim form inputs
+    $name = trim($_POST['name']);
+    $email = trim($_POST['email']);
+    $subject = trim($_POST['subject']);
+    $contact_message = trim($_POST['message']);
 
-   
-	if ($subject == '') { $subject = "Contact Form Submission"; }
+    // Set a default subject if none is provided
+    $subject = ($subject == '') ? "Contact Form Submission" : $subject;
 
-   // Set Message
-   $message .= "Email from: " . $name . "<br />";
-	 $message .= "Email address: " . $email . "<br />";
-   $message .= "Message: <br />";
-   $message .= nl2br($contact_message);
-   $message .= "<br /> ----- <br /> This email was sent from your site " . url() . " contact form. <br />";
+    // Build the email message
+    $message = "Email from: $name <br />";
+    $message .= "Email address: $email <br />";
+    $message .= "Message: <br />";
+    $message .= nl2br($contact_message);
+    $message .= "<br /> ----- <br /> This email was sent from your site " . getCurrentURL() . " contact form. <br />";
 
-   // Set From: header
-   $from =  $name . " <" . $email . ">";
+    // Set From: header
+    $from = "$name <$email>";
 
-   // Email Headers
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $email . "\r\n";
- 	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    // Email Headers
+    $headers = "From: $from\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-   ini_set("sendmail_from", $to); // for windows server
-   $mail = mail($to, $subject, $message, $headers);
+    // Try to send the email
+    $mail = mail($to, $subject, $message, $headers);
 
-	if ($mail) { echo "OK"; }
-   else { echo "Something went wrong. Please try again."; }
-
+    // Check if the email was sent successfully
+    if ($mail) {
+        echo "OK";
+    } else {
+        echo "Something went wrong. Please try again.";
+    }
 }
 
 ?>
